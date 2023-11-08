2c06c294e4cb61ee7cfcc09bb5e42cf2f0f3d2ef
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-11-08 15:32:10 +0100

    second commit

--- src/org/lessons/java/StampaSaluto.java
+++ src/org/lessons/java/StampaSaluto.java
@@ -2,6 +2,6 @@ package org.lessons.java;
 
 public class StampaSaluto {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!!!");
+		System.out.println("Hello, World!!!!");
 	}
 }
